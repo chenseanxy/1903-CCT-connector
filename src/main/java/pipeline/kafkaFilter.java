4b590156af26749475f23a5b7346f431216052fe
chenxy <git@chenxy.tech>
2019-05-04 19:24:37 +0800

    Beautify

--- src/main/java/pipeline/kafkaFilter.java
+++ src/main/java/pipeline/kafkaFilter.java
@@ -8,15 +8,19 @@ public class kafkaFilter {
     private static kafkaStream stream;
 
     public static void run(Properties props) {
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            public void run() {
-                kafkaFilter.cleanup();
-            }
-        });
+        Runtime.getRuntime().addShutdownHook(
+                new Thread(() -> kafkaFilter.cleanup())
+        );
 
-        stream = new kafkaStream(props.getProperty("kafka-server"), props.getProperty("kafka-topic"),
-                props.getProperty("kafka-topic-filtered"), "geofilter-stream-" + props.getProperty("kafka-appid"),
-                "geofilter-stream-client-" + props.getProperty("kafka-clientid"), (k, v) -> Record.jsonValid(v));
+        stream = new kafkaStream(
+                props.getProperty("kafka-server"),
+                props.getProperty("kafka-topic"),
+                props.getProperty("kafka-topic-filtered"),
+                "geofilter-stream-" + props.getProperty("kafka-appid"),
+                "geofilter-stream-client-" + props.getProperty("kafka-clientid"),
+                (k, v) -> Record.jsonValid(v)
+        );
+
         System.out.println("Initializing stream");
         stream.init();
         System.out.println("Starting stream");
